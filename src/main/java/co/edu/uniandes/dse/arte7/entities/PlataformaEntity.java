ce74e52bfd10fd1913bbae83fd9196c0c5efbb55
Juan Cuellar <j.cuellara@uniandes.edu.co>
2022-09-04 10:57:57 -0500

    PlataformaEntityIntento1

---
+++ src/main/java/co/edu/uniandes/dse/arte7/entities/PlataformaEntity.java
@@ -0,0 +1,29 @@
+package co.edu.uniandes.dse.arte7.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.FetchType;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Getter
+@Setter
+@Entity
+public class PlataformaEntity extends BaseEntity{
+
+    private String nombre;
+    private String url;
+
+    @PodamExclude
+    @ManyToMany(mappedBy = "peliculas", fetch = FetchType.LAZY)
+    private List<PlataformaEntity> plataformas = new ArrayList<>();
+ 
+}   

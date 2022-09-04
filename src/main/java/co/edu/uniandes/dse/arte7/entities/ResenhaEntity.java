ebcdfe8faa3b4972476c37ecd665c22ec877f840
Worfran <fraworman@hotmail.com>
2022-09-04 08:52:33 -0500

    Clase resenha

---
+++ src/main/java/co/edu/uniandes/dse/arte7/entities/ResenhaEntity.java
@@ -0,0 +1,23 @@
+package co.edu.uniandes.dse.arte7.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+
+public class ResenhaEntity extends BaseEntity{
+    private int estrellas;
+    private int numCaracteres;
+    private String texto;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "critico", fetch = FetchType.EAGER)
+    private usuarioEntity user;
+    
+
+}

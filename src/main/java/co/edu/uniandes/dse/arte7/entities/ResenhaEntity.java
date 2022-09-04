9d2a43f5e82079dfbc485b7506d0248375c7a1f4
CRISTIAN ACUÑA <cb.acuna@uniandes.edu.co>
2022-09-03 19:54:48 -0500

    UsuarioEntity creado

---
+++ src/main/java/co/edu/uniandes/dse/arte7/entities/ResenhaEntity.java
@@ -0,0 +1,9 @@
+package co.edu.uniandes.dse.arte7.entities;
+
+
+
+public class ResenhaEntity extends BaseEntity {
+    
+
+
+}
---
+++ src/main/java/co/edu/uniandes/dse/arte7/entities/UsuarioEntity.java
@@ -0,0 +1,20 @@
+package co.edu.uniandes.dse.arte7.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Getter
+@Setter
+@Entity
+
+public class UsuarioEntity extends BaseEntity {
+
+    @PodamExclude
+    @OneToMany
+    private ResenhaEntity resenha;
+
+}

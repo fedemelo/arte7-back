35e5e2aced1075e11e775b226e738d65e4c9097a
Worfran <fraworman@hotmail.com>
2022-09-04 14:41:40 -0500

    Repositorio Resenha

---
+++ src/main/java/co/edu/uniandes/dse/arte7/repositories/ResenhaRepository.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.dse.arte7.repositories;
+package co.edu.uniandes.dse.arte7.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.arte7.entities.ResenhaEntity;
+
+@Repository
+public class ResenhaRepository extends JpaRepository<ResenhaEntity, Long> {
+
+}
+

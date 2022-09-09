b1f98ea0da89ba86d13c4b1544e369d5960a4ac0
Federico Melo Barrero <f.melo@uniandes.edu.co>
2022-09-09 14:11:18 -0500

    Equals code smell fix

--- src/main/java/co/edu/uniandes/dse/arte7/entities/ActorEntity.java
+++ src/main/java/co/edu/uniandes/dse/arte7/entities/ActorEntity.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -19,6 +20,7 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Getter
 @Setter
 @Entity
+@EqualsAndHashCode(callSuper = true)
 public class ActorEntity extends TrabajadorEntity {
 
     @PodamExclude
--- src/main/java/co/edu/uniandes/dse/arte7/entities/UsuarioEntity.java
+++ src/main/java/co/edu/uniandes/dse/arte7/entities/UsuarioEntity.java
@@ -3,6 +3,7 @@ package co.edu.uniandes.dse.arte7.entities;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -10,6 +11,7 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Getter
 @Setter
 @Entity
+@EqualsAndHashCode(callSuper = true)
 public class UsuarioEntity extends BaseEntity {
 
     @PodamExclude

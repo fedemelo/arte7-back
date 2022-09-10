ce42112de38760ec326cf6a4e4c88c81d6fe9344
CRISTIAN ACUÑA <cb.acuna@uniandes.edu.co>
2022-09-10 04:45:42 -0500

    premiosservicecreada

---
+++ src/main/java/co/edu/uniandes/dse/arte7/services/PremioService.java
@@ -0,0 +1,21 @@
+package co.edu.uniandes.dse.arte7.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.arte7.repositories.PremioRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+
+public class PremioService {
+    
+    @Autowired
+    PremioRepository premioRepository;
+
+    
+
+
+
+}

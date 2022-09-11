4e1576ce5e48e822c3e414f5cb9ab50e51a683e5
Worfran <fraworman@hotmail.com>
2022-09-11 12:37:06 -0500

    F2

--- src/test/java/co/edu/uniandes/dse/arte7/services/PlataformaServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/arte7/services/PlataformaServiceTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
